d855692ce93f2d55b39f4883d696feb20261ee33
Jyoti yadav <jyoti.yadav2508@gmail.com>
2023-01-25 22:40:51 +0000

    Create superuser, install summernote and add url

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    """
+    Add fields which will use summernote editor in admin panel
+    """
+    summernote_fields = ('content')
