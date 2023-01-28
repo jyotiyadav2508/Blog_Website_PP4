5dab4a4f0f5611358c6563afbb73c957eb848e68
Jyoti yadav <jyoti.yadav2508@gmail.com>
2023-01-28 15:22:26 +0000

    Insert image and logo in header in base.html

--- blog/views.py
+++ blog/views.py
@@ -10,5 +10,5 @@ class PostList(generic.ListView):
     """
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = index.html
+    template_name = "index.html"
     paginate_by = 6
