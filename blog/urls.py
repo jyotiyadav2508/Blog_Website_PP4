a0bdf53ae0a80c8a8471b1ca28dbc8540b93b45e
Jyoti yadav <jyoti.yadav2508@gmail.com>
2023-02-02 12:34:10 +0000

    Add card-footer class in blog card

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>/', views.PostLike.as_view(), name='post_like'),
-    path('add_post', views.AddPost.as_view(), name='add-post'),
+    path('add_post', views.AddPost.as_view(), name='add_post'),
+    path('blog', views.AllBlogPost.as_view(), name='all_blog'),
 ]
