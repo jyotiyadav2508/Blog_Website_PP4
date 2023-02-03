39093f47dd76c3c139b094a3b00a2027b571ef74
Jyoti yadav <jyoti.yadav2508@gmail.com>
2023-02-03 14:06:48 +0000

    Add sign in link in post_detail template

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     # path('post_detail', views.PostDetail.as_view(), name='post_detail'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>/', views.PostLike.as_view(), name='post_like'),
-    path('add_post', views.AddPost.as_view(), name='add-post'),
     path('blog', views.AllBlogPost.as_view(), name='all-blog'),
     path('user_page', views.User.as_view(), name="user-page"),
+    path('add_post', views.AddPost.as_view(), name='add-post'),
 ]
